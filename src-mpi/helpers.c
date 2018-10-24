d4937d47bc8532390fe3d92e4d261c10a4522317
Emilio J. Padrón González <emilioj@udc.gal>
2018-10-24 22:29:58 +0200

    Add statistics helpers
    From Omar A. Mures' original commit f9da6875e4d[...]

--- src-mpi/helpers.c
+++ src-mpi/helpers.c
@@ -4,9 +4,84 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <math.h>
 #include <sys/types.h>
 
 #include "helpers.h"
+
+void dump_times(array_t data, char * filename) {
+
+  FILE *f = fopen(filename, "w");
+  if (f == NULL) {
+      printf("Error opening stats file!\n");
+      exit(1);
+  }
+
+  fprintf(f, "#Step Time\n");
+  for (int i = 0; i < kv_size(data); ++i)
+    fprintf(f, "%d %f\n", i, kv_A(data,i));
+
+  fclose(f);
+
+}
+
+void dump_stats(array_t data, char * filename) {
+
+  FILE *f = fopen(filename, "w");
+  if (f == NULL) {
+      printf("Error opening stats file!\n");
+      exit(1);
+  }
+
+  float mean = avg(data);
+
+  fprintf(f, "#Average Max Min StdDev\n");
+  fprintf(f, "%f %f %f %f\n", avg(data), max(data),  min(data), std_dev(data, mean));
+
+  fclose(f);
+
+}
+
+float std_dev(array_t data, float mean) {
+
+  float std_dev = 0.f;
+
+  for (size_t i = 0; i < kv_size(data); ++i)
+    std_dev += pow(kv_A(data, i) - mean, 2);
+
+  return sqrt(std_dev/kv_size(data));
+
+}
+
+float avg(array_t data) {
+
+  float avg = 0.f;
+  for (size_t i = 0; i < kv_size(data); i++)
+    avg += kv_A(data, i);
+
+  return avg/kv_size(data);
+
+}
+
+float max(array_t data) {
+
+  float max = kv_A(data, 0);
+  for (size_t i = 0; i < kv_size(data); i++)
+    max = max < kv_A(data, i) ? kv_A(data, i) : max;
+
+  return max;
+
+}
+
+float min(array_t data) {
+
+  float min = kv_A(data, 0);
+  for (size_t i = 0; i < kv_size(data); i++)
+    min = min > kv_A(data, i) ? kv_A(data, i) : min;
+
+  return min;
+
+}
 
 void check_hostname(char * read_hostname){
 
--- src-mpi/helpers.h
+++ src-mpi/helpers.h
@@ -1,9 +1,25 @@
 #ifndef HELPERS_H_   /* Include guard */
 #define HELPERS_H_
 
+#include <time.h>
+#include "kvec.h"
+
 #define MAX_CHARS_KEY 1000
 #define MAX_MESSAGE_SIZE 2048 //2 KB
 #define MAX_CHARS_HOSTNAME 1024
+
+#define START_TIMING(t) clock_t t = clock()
+#define END_TIMING(t) (float)(clock() - t) / CLOCKS_PER_SEC
+
+typedef kvec_t(float) array_t;
+
+float std_dev(array_t data, float mean);
+float avg(array_t data);
+float max(array_t data);
+float min(array_t data);
+
+void dump_times(array_t data, char * filename);
+void dump_stats(array_t data, char * filename);
 
 void check_hostname(char * read_hostname);
 
@@ -11,6 +27,6 @@ char * read_hostname(char * path, char * file);
 
 char ** read_dir(char * path, unsigned int * length);
 
-char* rand_string_alloc(size_t size);
+char * rand_string_alloc(size_t size);
 
 #endif // HELPERS_H_
